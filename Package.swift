b38d9be50dbd07109d20c4bd2f0d5cb2fac4617b
Mark Chan <memz1@darock.top>
2026-01-17 20:19:58 +0800

    Update CryptorECC package declaration format

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     dependencies: [
         .package(name: "CryptorRSA", url: "https://github.com/Kitura/BlueRSA.git", from: "1.0.202"),
         .package(name: "Cryptor", url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.1"),
-        .package(name: "CryptorECC", url: "https://github.com/Kitura/BlueECC.git", branch: "master"),
+        .package(url: "https://github.com/Kitura/BlueECC.git", branch: "master"),
         .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "2.0.0"),
         .package(url: "https://github.com/Kitura/KituraContracts.git", from: "2.0.1")
     ],
